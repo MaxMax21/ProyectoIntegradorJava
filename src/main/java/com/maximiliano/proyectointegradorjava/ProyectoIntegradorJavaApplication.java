f2ceeca356faf4947d29f965fe9e9b3376c9a24d
Maximiliano Rodriguez <rodriguezmaximiliano4@gmail.com>
2022-07-09 21:12:34 -0300

    First Commit

---
+++ src/main/java/com/maximiliano/proyectointegradorjava/ProyectoIntegradorJavaApplication.java
@@ -0,0 +1,13 @@
+package com.maximiliano.proyectointegradorjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoIntegradorJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maximiliano/proyectointegradorjava/ProyectoIntegradorJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximiliano.proyectointegradorjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
